b07931fc794419b44d87906e66268dffe7ed5a23
rodrigore <rodrigopcg@gmail.com>
2016-04-30 14:58:26 -0300

    Clean tmux conf

--- scripts/tmux_start.sh
+++ scripts/tmux_start.sh
@@ -11,7 +11,7 @@ tmux has-session -t default || tmux new-session -s default -d
 
 # present menu for user to choose which workspace to open
 PS3="Please choose your session: "
-options=($(tmux list-sessions -F "#S") "NEW SESSION" "Vagrant" "Vida_Sana" "ZSH")
+options=($(tmux list-sessions -F "#S") "NEW SESSION" "ZSH")
 echo "Available sessions"
 echo "------------------"
 echo " "
@@ -21,20 +21,6 @@ do
         "NEW SESSION")
             read -p "Enter new session name: " SESSION_NAME
             tmux new -s "$SESSION_NAME"
-            break;;
-        "ens")
-            tmux new -s "Vagrant"
-            cd ~/ens-vagrant/
-            tmux new-window -n "Vim" "vim"
-            tmux new-window -n "Vagrant"
-            tmux new-window -n "Git"
-            tmux split-window -h
-            break;;
-        "Vida_Sana")
-            tmux new -s "Vida_Sana"
-            tmux new-window -n "Weechat" "weechat"
-            tmux new-window -n "Vagrant" "cd ~/VagrantTest/ && vagrant up && vagrant ssh"
-            tmux split-window -h -c ~/VagrantTest/vida_sana/ vim
             break;;
         "ZSH")
             zsh --login
